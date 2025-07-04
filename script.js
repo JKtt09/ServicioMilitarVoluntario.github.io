3cb3fe445cc2c0da6002394f337f405a9a950518
Jktt0908 <manisillojuaquin@gmail.com>
2025-07-04 17:18:01 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Agregar evento al botón de inscripción
+document.querySelector('button').addEventListener('click', () => {
+    // Redirigir a la página de inscripción
+    window.location.href = 'inscripcion.html';
+});
